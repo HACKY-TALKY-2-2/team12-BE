bdb46371eb6f9e0dd47df731a67e844fcf6a2928
bumstead-bumstead <dldyghks951@gmail.com>
2023-11-10 20:42:53 +0900

    initial commit

---
+++ src/main/java/com/hackytalky/team12server/Team12serverApplication.java
@@ -0,0 +1,13 @@
+package com.hackytalky.team12server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team12serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team12serverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackytalky/team12server/Team12serverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackytalky.team12server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team12serverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
